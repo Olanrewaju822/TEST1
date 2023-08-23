eb8984b5542977a6a9f4f82ef1646057f9255d09
Olanrewaju822 <aladeolanrewaju822@gmail.com>
2023-08-23 15:49:24 +0100

    Olanrewaju822

---
+++ admin_page.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin page</title>
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <div class="container">
+
+    <div class="content">
+        <h3>  Hi, <span> Admin </span> </h3>
+        <h1> Welcome <span> </span> </h1>
+        <p> This is an Admin Page</p>
+        <a href="login_form.php" class="btn"> Login </a>
+        <a href="register_form.php" class="btn"> Register </a>
+        <a href="logout_form.php" class="btn"> Logout </a>
+    
+
+    </div>
+</div>
+
+<script src="index.js"></script>
+
+</body>
+</html>
